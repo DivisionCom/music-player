203e82396695d10113aba9e1c371377c27be2639
DivisionCom <xd6511@gmail.com>
2023-11-26 21:02:45 +0300

    Add Progress Box & Add Status Box under image card

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -20,16 +20,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -40,6 +44,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -47,6 +52,7 @@ import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.fontscaling.MathUtils.lerp
 import androidx.compose.ui.unit.sp
@@ -221,6 +227,46 @@ class MainActivity : ComponentActivity() {
                                 }
 
 
+                            }
+                            Spacer(modifier = Modifier.height(54.dp))
+                            Row(
+                                modifier = Modifier.padding(horizontal = 24.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "00:00",
+                                    modifier = Modifier.width(55.dp),
+                                    color = textColor,
+                                    textAlign = TextAlign.Center
+                                )
+                                // Progress Box
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .height(8.dp)
+                                        .padding(horizontal = 8.dp)
+                                        .clip(CircleShape)
+                                        .background(Color.White),
+                                    contentAlignment = Alignment.CenterStart
+                                ) {
+                                    // Status Box
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth(fraction = .5f)
+                                            .fillMaxHeight()
+                                            .clip(
+                                                RoundedCornerShape(8.dp)
+                                            )
+                                            .background(Color(0xff414141))
+                                    )
+                                }
+                                Text(
+                                    text = "00:00",
+                                    modifier = Modifier.width(55.dp),
+                                    color = textColor,
+                                    textAlign = TextAlign.Center
+                                )
                             }
 
                         }
